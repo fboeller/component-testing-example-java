7686b88b7ff505db8f4f8620715c352e939bf8cf
fboeller <fabianboeller@googlemail.com>
2020-03-01 18:49:19 +0100

    Init database and add two tests

--- src/test/java/RunDAOTest.java
+++ src/test/java/RunDAOTest.java
@@ -1,3 +1,7 @@
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -9,12 +13,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RunDAOTest {
 
     @Container
-    private static PostgreSQLContainer container = new PostgreSQLContainer("postgres:11.5");
+    private static PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:11.5");
 
-    @Test
-    public void t1() {
-        assertThat(container.isRunning())
-                .isTrue();
+    private static Jdbi jdbi;
+
+    @BeforeAll
+    public static void setupDatabase() throws Exception {
+        jdbi = Database.initDatabase(container.getJdbcUrl(), container.getUsername(), container.getPassword());
     }
 
+    @AfterEach
+    public void cleanDatabase() {
+        jdbi.useHandle(handle -> handle.execute("TRUNCATE run"));
+    }
+
+    @Test
+    @DisplayName("No runs are selected when no runs exist")
+    void t1() {
+        jdbi.useExtension(RunDAO.class, dao ->
+                assertThat(dao.selectRuns()).isEmpty()
+        );
+    }
+
+    @Test
+    @DisplayName("A single run is selected when it is inserted")
+    void t2() {
+        jdbi.useExtension(RunDAO.class, dao -> {
+            var id = dao.createRun();
+            assertThat(dao.selectRuns())
+                    .containsExactly(new Run(id, "RUNNING"));
+        });
+    }
 }
