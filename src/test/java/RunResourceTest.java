ba6f9f19d94987df8121e93f210c27b743af7964
fboeller <fabianboeller@googlemail.com>
2020-03-01 19:42:51 +0100

    Use PostgresContainerExtension in RunResourceTest

--- src/test/java/RunResourceTest.java
+++ src/test/java/RunResourceTest.java
@@ -1,37 +1,25 @@
-import org.jdbi.v3.core.Jdbi;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Testcontainers
 public class RunResourceTest {
 
-    @Container
-    private static PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:11.5");
+    @RegisterExtension
+    public static PostgresTestcontainerExtension db = new PostgresTestcontainerExtension();
 
-    private static Jdbi jdbi;
     private static RunResource runResource;
 
     @BeforeAll
-    public static void beforeAll() throws Exception {
-        jdbi = Database.initDatabase(container.getJdbcUrl(), container.getUsername(), container.getPassword());
+    public static void beforeAll() {
         var runService = mock(RunService.class);
         when(runService.executeRun(anyInt())).thenReturn(true);
-        runResource = new RunResource(jdbi, runService);
-    }
-
-    @AfterEach
-    public void cleanDatabase() {
-        jdbi.useHandle(handle -> handle.execute("TRUNCATE run"));
+        runResource = new RunResource(db.getJdbi(), runService);
     }
 
     @Test
