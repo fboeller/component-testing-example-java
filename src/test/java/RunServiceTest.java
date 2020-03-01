71780149b81942a4b2eff2cf4e788fb7e11c5114
fboeller <fabianboeller@googlemail.com>
2020-03-01 19:24:24 +0100

    Add three more tests to RunServiceTest

--- src/test/java/RunServiceTest.java
+++ src/test/java/RunServiceTest.java
@@ -3,13 +3,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockserver.client.MockServerClient;
+import org.mockserver.model.HttpError;
 import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockserver.model.HttpError.error;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.verify.VerificationTimes.exactly;
 import static org.mockserver.verify.VerificationTimes.once;
 
 @Testcontainers
@@ -40,6 +43,31 @@ public class RunServiceTest {
         var isSuccess = runService.executeRun(1);
         assertThat(isSuccess).isTrue();
         mockServerClient.verify(request().withMethod("POST").withPath("/item"), once());
+    }
+
+    @Test
+    @DisplayName("Multiple runs result successful requests")
+    public void t2() {
+        mockServerClient.when(request()).respond(response().withStatusCode(200));
+        var isSuccess = runService.executeRun(5);
+        assertThat(isSuccess).isTrue();
+        mockServerClient.verify(request().withMethod("POST").withPath("/item"), exactly(5));
+    }
+
+    @Test
+    @DisplayName("A run fails on a server error")
+    public void t3() {
+        mockServerClient.when(request()).respond(response().withStatusCode(500));
+        var isSuccess = runService.executeRun(1);
+        assertThat(isSuccess).isFalse();
+    }
+
+    @Test
+    @DisplayName("A run fails on a connection error")
+    public void t4() {
+        mockServerClient.when(request()).error(error().withDropConnection(true));
+        var isSuccess = runService.executeRun(2);
+        assertThat(isSuccess).isFalse();
     }
 
 }
