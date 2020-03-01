f6c8cb1d30055e803db410fdd9c0c751642aa6c9
fboeller <fabianboeller@googlemail.com>
2020-03-01 19:57:31 +0100

    Use single postgres container for all tests

--- src/test/java/PostgresTestcontainerExtension.java
+++ src/test/java/PostgresTestcontainerExtension.java
@@ -1,22 +1,24 @@
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-public class PostgresTestcontainerExtension implements BeforeAllCallback, AfterEachCallback {
+public class PostgresTestcontainerExtension implements AfterEachCallback {
 
-    private Jdbi jdbi;
+    static {
+        var container = new PostgreSQLContainer<>("postgres:11.5");
+        container.start();
+        try {
+            jdbi = Database.initDatabase(container.getJdbcUrl(), container.getUsername(), container.getPassword());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Jdbi jdbi;
 
     public Jdbi getJdbi() {
         return jdbi;
-    }
-
-    @Override
-    public void beforeAll(ExtensionContext extensionContext) throws Exception {
-        var container = new PostgreSQLContainer<>("postgres:11.5");
-        container.start();
-        jdbi = Database.initDatabase(container.getJdbcUrl(), container.getUsername(), container.getPassword());
     }
 
     @Override
